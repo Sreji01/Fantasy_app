40be6811219451be8f5a8401bc6d8381c0d0d368
Sreji01 <marko.srejic01@gmail.com>
2024-11-18 12:29:09 +0100

    Ubaceni midje testovi u projekat.

--- src/fantasy_app/core.clj
+++ src/fantasy_app/core.clj
@@ -1,8 +1,16 @@
 (ns fantasy-app.core
+(:require [midje.sweet :refer :all])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  
   (println "Hello, World!"))
 
+(defn calculate-player-predicted-points
+"A function that calculates player's predicted points based in next gameweek"
+[player])
+
+(fact "Calculates player's points"
+(calculate-player-predicted-points nil) =not=> nil)
