a67a5b2a0ea1faa36950bcf3e11eb38c3f6aab32
Sreji01 <marko.srejic01@gmail.com>
2024-11-18 13:48:31 +0100

    Promenjena metoda calculate predicted points da ima povratnu vrednost.

--- src/fantasy_app/core.clj
+++ src/fantasy_app/core.clj
@@ -8,9 +8,21 @@
   
   (println "Hello, World!"))
 
+(def player
+  {:id 101 :position 4 :xg 0.15 :xa 0.08 :xgc 0.90 
+  :chance-of-playing 95 :clean-sheet-probability 0.0 :bps-rank 10 
+  :form 5.4 
+  :minutes-per-game 85 :ict-index 45.0 :expected-bonus 3})
+
+
 (defn calculate-player-predicted-points
 "A function that calculates player's predicted points based in next gameweek"
-[player])
+[player]
+(let [{:keys [xg xa expected-bonus]} player
+      goal-points (* 4 xg)
+      assist-points (* 3 xa)
+      bonus-points (* 1 expected-bonus)]
+      (+ goal-points assist-points bonus-points)))
 
 (fact "Calculates player's points"
 (calculate-player-predicted-points nil) =not=> nil)
